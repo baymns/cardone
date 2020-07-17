ffe8155de3ffdcd998a7b3dd04e2d5d380eef0f7
Mansur <baymns10@gmail.com>
2020-07-17 22:38:21 +0300

    add home links and adaptive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
 import Home from './components/Home/index';
 
