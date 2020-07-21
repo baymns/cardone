b384cc8d6010386dd81dbe43785f7df4c90c7074
Vadyton <vadyton@yandex.ru>
2020-07-22 00:02:23 +0300

    Проведен рефакторинг. Так же бот теперь сохраняет информацию о принятых заявках на эвакуацию в БД и оповещает в случае, если заявка уже в работе

--- botServer/routes/evacuatorOrder.js
+++ botServer/routes/evacuatorOrder.js
@@ -13,26 +13,51 @@ const bot = new TelegramBot(`${process.env.TOKEN}`, {
   polling: true,
 });
 
-mongoose.connect('mongodb://localhost:27017/cardone', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
+mongoose.connect(
+  `mongodb+srv://Sergey:${process.env.MONGO_DB_KEY}@cluster0-aqh9k.mongodb.net/service?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+);
 
 bot.on('message', userStartAdder);
 
 router.post('/', async (req, res) => {
-  console.log(req.body);
-  const { username, phone, brand, model, address } = req.body;
+  const { numberOfOrder, username, phone, brand, model, address } = req.body;
   const allDrivers = await EvacuatorBotUser.find();
   allDrivers.map((person) =>
     bot.sendMessage(
       person.telegram_id,
-      `Поступил новый заказ: Имя заказчика: ${username}, Номер телефона: ${phone}, Автомобиль: ${brand} ${model}, Адрес: ${address}`,
+      `Поступил новый заказ: Номер заказа: ${numberOfOrder} Имя заказчика: ${username}, Номер телефона: ${phone}, Автомобиль: ${brand} ${model}, Адрес: ${address}`,
     ),
   );
-  return res.json('Заявка на эвакуацию поступила исполнителям');
+  return res.end();
+});
+
+bot.on('message', async (msg) => {
+  if (msg.text.toLowerCase().match('принял заказ')) {
+    const number = msg.text.match(/\d{4,7}/gm).join();
+    const test = await EvacuatorOrder.findOne({
+      numberOfOrder: number,
+    });
+    if (test.inProgress === false) {
+      const driverId = await EvacuatorBotUser.findOne({
+        telegram_id: msg.from.id,
+      });
+      await EvacuatorOrder.findOneAndUpdate(
+        {
+          numberOfOrder: number,
+        },
+        { $set: { inProgress: true, driver: driverId } },
+      );
+      bot.sendMessage(msg.from.id, '✅Отлично! Работаем!');
+    } else {
+      bot.sendMessage(msg.from.id, '❌Заказ уже в работе');
+    }
+  }
 });
 
 export default router;
