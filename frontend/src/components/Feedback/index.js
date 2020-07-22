70cbdb89d8fec2030f3fc8f68b20f9de976a4949
Vadyton <vadyton@yandex.ru>
2020-07-22 13:17:18 +0300

    First check uploader

--- frontend/src/components/Feedback/index.js
+++ frontend/src/components/Feedback/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './feedback.module.scss';
+import Uploader from '../Uploader';
 
 function Feedback({ name }) {
   return (
@@ -9,6 +10,7 @@ function Feedback({ name }) {
         <div>Оцените это место</div>
         <div>⭐⭐⭐⭐⭐</div>
         <div>Комментарии</div>
+        <Uploader />
         <input placeholder="Поделитесь мнением про плюсы и минусы этого места"></input>
         <button type="submit">Отправить</button>
         <button>Отменить</button>
