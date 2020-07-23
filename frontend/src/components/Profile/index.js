14f0d64f81ef260b4f1d5ace0e0ecb502e92ad29
Sergey <sergeistrelkov98@gmail.com>
2020-07-23 21:57:20 +0300

    avatar some

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -3,12 +3,15 @@ import { useSelector } from 'react-redux';
 import defaultAvatar from '../../images/default-avatar.png';
 import styles from './profile.module.scss';
 import FotoEditor from '../Foto';
-
+import Favourites from './Favourites';
 
 function Profile() {
   const [uploadFoto, setUploadFoto] = useState(false);
+  const [showChosen, setShowChosen] = useState(false);
   const user = useSelector((state) => state.user);
   let loadedFile = user.avatar;
+  const { favourites } = user;
+  console.log(favourites);
   return (
     <div className={styles.profile_container}>
       <div className={styles.avatar}>
@@ -20,14 +23,17 @@ function Profile() {
         <div>
           <p>{user.name}</p>
           <p>{user.email}</p>
+          <button onClick={() => setShowChosen(!showChosen)}>Избранное</button>
         </div>
       </div>
-      
+
       <div className="options"></div>
 
       {uploadFoto && (
         <FotoEditor userId={user.id} setUploadFoto={setUploadFoto} />
       )}
+      {showChosen &&
+        favourites.map((service) => <Favourites key={service.id} service={service} userId={user.id} />)}
     </div>
   );
 }
